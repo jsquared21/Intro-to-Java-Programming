024d33fe246f7aa8cc56bf9551609c4f9c197128
jsquared21 <jabarimail@gmail.com>
2015-06-03 22:16:02 -0400

    Add Ex 5.14

--- Exercise_05/Exercise_05_14/Exercise_05_14.java
+++ Exercise_05/Exercise_05_14/Exercise_05_14.java
@@ -6,3 +6,29 @@ a divisor for both n1 and n2 in this order. The first such common divisor is the
 greatest common divisor for n1 and n2. Write a program that prompts the user to
 enter two positive integers and displays the gcd.
 */
+import java.util.Scanner;
+
+public class Exercise_05_14 {
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter two integers
+		System.out.print("Enter first integer: ");
+		int n1 = input.nextInt();
+		System.out.print("Enter second integer: ");
+		int n2 = input.nextInt();
+
+		// Make gcd to be the minimum of n1 and n2
+		int gcd = n1 < n2 ? n1 : n2;
+
+		// Find the greatest common divisor
+		while (n1 % gcd != 0 || n2 % gcd != 0) {
+			gcd--;
+		}
+
+		// Display result
+		System.out.println("The greatest common divisor for " + n1 +
+			" and " + n2 + " is " + gcd);
+	}
+}
