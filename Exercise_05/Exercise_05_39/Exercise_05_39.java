fd45ab113c55813a02d96b9020f0be5000efee3c
jsquared21 <jabarimail@gmail.com>
2015-06-06 13:48:45 -0400

    Edit comments Ex 5.39

--- Exercise_05/Exercise_05_39/Exercise_05_39.java
+++ Exercise_05/Exercise_05_39/Exercise_05_39.java
@@ -29,15 +29,15 @@ public class Exercise_05_39 {
 			balance = commission = 0;	// Set balance and commission to 0
 			salesAmount += 0.01;			// Increase sales amount by $0.01
 
-			// If salest amount is $10,000.01 and above commission rate is 12%
+			// If sales amount is $10,000.01 and above commission rate is 12%
 			if (salesAmount > 10000)
 				commission += (balance = salesAmount - 10000) * 0.12;
 
-			// If salest amount is $5,000.01-$10,000 commission rate is 10%
+			// If sales amount is $5,000.01-$10,000 commission rate is 10%
 			if (salesAmount > 5000)
 				commission += (balance -= balance - 5000) * 0.10;
 
-			// If salest amount is $0.01-$5,000 commission rate is 8%
+			// If sales amount is $0.01-$5,000 commission rate is 8%
 			if (salesAmount > 0)
 				commission += balance * 0.08;
 
