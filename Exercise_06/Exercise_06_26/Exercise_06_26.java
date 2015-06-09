65573b13fd04c0e927331a17209ef6c8b1c7b049
jsquared21 <jabarimail@gmail.com>
2015-06-08 22:48:35 -0400

    Modify Ex 6.26

--- Exercise_06/Exercise_06_26/Exercise_06_26.java
+++ Exercise_06/Exercise_06_26/Exercise_06_26.java
@@ -15,18 +15,21 @@ public class Exercise_06_26 {
 		final int NUMBERS_PER_LINE = 10; // Display 10 numbers per line
 
 		int count = 0; // Counts the number of palindromic prime numbers
-		int index = 2;	// Possible palindromic prime numbers
+		int n = 2;	// Possible palindromic prime numbers
 
 		// Display 10 numbers per line, separated by exactly one space
 		while (count < NUMBER_OF_PALINDROMIC_PRIMES) {
-			if (PrimeNumberMethod.isPrime(index) && 
-				 Exercise_06_03.isPalindrome(index)) {
+			if (isPalindromicPrime(n)) {
 				count++; // Increment count
-			
-				System.out.print(count % NUMBERS_PER_LINE == 0 ? 
-					index + "\n" : index + " ");
+				System.out.print(count % NUMBERS_PER_LINE == 0 ? n + "\n" : n + " ");
 			}
-			index++; // Increment index
+			n++; // Increment index
 		}
+	}
+
+	/** Method isPalindromicPrime */
+	public static boolean isPalindromicPrime(int num) {
+		return PrimeNumberMethod.isPrime(num) && 
+				 Exercise_06_03.isPalindrome(num);
 	}
 }
