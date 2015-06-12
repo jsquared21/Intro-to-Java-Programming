d558d8c64f81735951a4880f1cca1c6ee2c89e6d
jsquared21 <jabarimail@gmail.com>
2015-06-12 19:28:49 -0400

    Add Ex 7.5

---
+++ Exercise_07/Exercise_07_05/Exercise_07_05.java
@@ -0,0 +1,50 @@
+/*
+(Print distinct numbers) Write a program that reads in ten numbers and displays
+the number of distinct numbers and the distinct numbers separated by exactly one
+space (i.e., if a number appears multiple times, it is displayed only once). (Hint:
+Read a number and store it to an array if it is new. If the number is already in the
+array, ignore it.) After the input, the array contains the distinct numbers.
+*/
+import java.util.Scanner;
+
+public class Exercise_07_05 {
+	/** Main Method */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int[] distinctNumbers = new int[10]; // Array of length 10;
+		int num; 				// User input
+		int count = 0;			// Number of distinct numbers
+
+		// Prompt the user to enter ten numbers
+		System.out.print("Enter ten numbers: ");
+		for (int i = 0; i < 10; i++) {
+			num = input.nextInt();
+
+			// Test if num is distinct
+			if (isDistinct(distinctNumbers, num)) {
+				distinctNumbers[i] = num;
+				count++;	// Increment count
+			}
+		}
+
+		// Displays the number of distinct numbers and the  
+		// distinct numbers separated by exactly one space
+		System.out.println("The number of distinct numbers is " + count);
+		System.out.print("The distinct numbers are");
+		for (int i = 0; i < distinctNumbers.length; i++) {
+			if (distinctNumbers[i] > 0)
+				System.out.print(" " + distinctNumbers[i]);
+		}
+		System.out.println();
+	}
+
+	/** Method isDistinct returns true if number is not in array false otherwise */
+	public static boolean isDistinct(int[] array, int num) {
+		for (int i = 0; i < array.length; i++) {
+			if (num == array[i]) 
+				return false;
+		}
+		return true;
+	}
+}
