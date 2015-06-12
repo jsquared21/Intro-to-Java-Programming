a0f3d71ccff56652637bcabb6f30a3afe391e81f
jsquared21 <jabarimail@gmail.com>
2015-06-11 22:43:46 -0400

    Ex 7.2

---
+++ Exercise_07/Exercise_07_02/Exercise_07_02.java
@@ -0,0 +1,6 @@
+/*
+(Reverse the numbers entered) Write a program that reads ten integers and displays
+them in the reverse of the order in which they were read.
+(Count occurrence of numbers) Write a program that reads the integers between 1
+and 100 and counts the occurrences of each. Assume the input ends with 0. 
+*/
