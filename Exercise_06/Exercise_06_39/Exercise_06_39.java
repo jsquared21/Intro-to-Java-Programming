96223e8a9544ccdb7662b897c52ffa91d2ed8b9b
jsquared21 <jabarimail@gmail.com>
2015-06-22 19:27:01 -0400

    Modify Ex 6.39 comments

--- Exercise_06/Exercise_06_39/Exercise_06_39.java
+++ Exercise_06/Exercise_06_39/Exercise_06_39.java
@@ -52,26 +52,26 @@ public class Exercise_06_39 {
 	* directed line from (x0, y0) to (x1, y1) */
 	public static boolean leftOfTheLine(double x0, double y0,
 	double x1, double y1, double x2, double y2){
-		return pointPoistion(x0, y0, x1, y1, x2, y2) > 0;
+		return pointPosition(x0, y0, x1, y1, x2, y2) > 0;
 	}
 
 	/** Return true if point (x2, y2) is on the same
 	* line from (x0, y0) to (x1, y1) */
 	public static boolean onTheSameLine(double x0, double y0,
 	double x1, double y1, double x2, double y2){
-		return pointPoistion(x0, y0, x1, y1, x2, y2) == 0;
+		return pointPosition(x0, y0, x1, y1, x2, y2) == 0;
 	}
 
 	/** Return true if point (x2, y2) is on the
 	* line segment from (x0, y0) to (x1, y1) */
 	public static boolean onTheLineSegment(double x0, double y0,
 	double x1, double y1, double x2, double y2) {
-		return (pointPoistion(x0, y0, x1, y1, x2, y2) == 0 &&
+		return (pointPosition(x0, y0, x1, y1, x2, y2) == 0 &&
 				 (x2 > x0) && (y2 > y0) && (x2 < x1) && (y2 < y1));
 	}
 
-	/** Method pointPosition returns the postion of p2 to p0 and p1 */
-	public static double pointPoistion(double x0, double y0,
+	/** Method pointPosition returns the position of p2 to p0 and p1 */
+	public static double pointPosition(double x0, double y0,
 	double x1, double y1, double x2, double y2) {
 		// Calculate point position
 		return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
