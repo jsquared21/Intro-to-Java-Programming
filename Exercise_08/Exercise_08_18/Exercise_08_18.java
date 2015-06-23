5afd2dc05d42e4cfe4735df9627d8c635f89c2a3
jsquared21 <jabarimail@gmail.com>
2015-06-22 23:21:16 -0400

    Modify comments Ex 8.18

--- Exercise_08/Exercise_08_18/Exercise_08_18.java
+++ Exercise_08/Exercise_08_18/Exercise_08_18.java
@@ -11,8 +11,13 @@
 public class Exercise_08_18 {
 	/** Main method */
 	public static void main(String[] args) {
+		// Initialize matrix
 		int[][] m = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+
+		// Randomly shuffle the rows in the matrix
 		shuffle(m);
+
+		// Display the matrix
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
 				System.out.print(m[i][j] + " ");
@@ -21,11 +26,15 @@ public class Exercise_08_18 {
 		}
 	}
 
+	/** shuffle shuffles the rows in a two-dimensional int array */
 	public static void shuffle(int[][] m) {
 		int[] temp = new int[2];
+
 		for (int i = 0; i < m.length; i++) {
+			// Generate random index
 			int i1 = (int)(Math.random() * m.length);
 
+			// Swap row i with row i1
 			temp[0] = m[i][0];
 			temp[1] = m[i][1];
 			m[i][0] = m[i1][0];
