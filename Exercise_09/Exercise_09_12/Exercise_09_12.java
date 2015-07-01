0da03488199cae4f75a5cbe72fe605b736e57654
jsquared21 <jabarimail@gmail.com>
2015-07-01 00:32:05 -0400

    Modify comments Ex 9.12

--- Exercise_09/Exercise_09_12/Exercise_09_12.java
+++ Exercise_09/Exercise_09_12/Exercise_09_12.java
@@ -17,6 +17,7 @@ public class Exercise_09_12 {
 		System.out.print("Enter x1, y1, x2, y2, x3, y3, x4, y4: ");
 		double[][] points = getPoints();
 
+		// Get a, b, c, d, e and f
 		double[] p = getArguments(points);
 
 		// Create a LinearEquation object
