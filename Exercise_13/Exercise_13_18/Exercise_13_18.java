67cd7e8bfd4360d781fff7860c6c57b2542ee012
jsquared21 <jabarimail@gmail.com>
2015-08-07 00:13:19 -0400

    Add Ex 13.18

---
+++ Exercise_13/Exercise_13_18/Exercise_13_18.java
@@ -0,0 +1,25 @@
+/*********************************************************************************
+* (Use the Rational class) Write a program that computes the following summation *
+* series using the Rational class:                                               *
+*                                                                                *
+*          1 / 2 + 2 / 3 + 3 / 4 + ... + 98 / 99 + 99 / 100                      *
+*                                                                                *
+* You will discover that the output is incorrect because of integer overflow     *
+* (too large). To fix this problem, see Programming Exercise 13.15.              *
+*********************************************************************************/
+import java.math.BigInteger;
+
+public class Exercise_13_18 {
+	/** Main method */
+	public static void main(String[] args) {
+		Rational r = new Rational(BigInteger.ONE, new BigInteger("2"));
+
+		for (BigInteger i = r.getNumerator().add(BigInteger.ONE);
+			  i.compareTo(new BigInteger("100")) <= 0;
+			  i = i.add(BigInteger.ONE)) {
+			r = r.add(new Rational(i, i.add(BigInteger.ONE)));
+		}
+
+		System.out.println(r);
+	}
+}
