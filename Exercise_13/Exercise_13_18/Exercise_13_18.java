75045edb3fe71f50da874e9c5e3f650ddeb697b3
jsquared21 <jabarimail@gmail.com>
2015-08-07 00:17:08 -0400

    Add comments

--- Exercise_13/Exercise_13_18/Exercise_13_18.java
+++ Exercise_13/Exercise_13_18/Exercise_13_18.java
@@ -12,14 +12,17 @@ import java.math.BigInteger;
 public class Exercise_13_18 {
 	/** Main method */
 	public static void main(String[] args) {
+		// Create a Rational object
 		Rational r = new Rational(BigInteger.ONE, new BigInteger("2"));
 
+		// Add a series of Rationol objects to r
 		for (BigInteger i = r.getNumerator().add(BigInteger.ONE);
 			  i.compareTo(new BigInteger("100")) <= 0;
 			  i = i.add(BigInteger.ONE)) {
 			r = r.add(new Rational(i, i.add(BigInteger.ONE)));
 		}
 
+		// Display results
 		System.out.println(r);
 	}
 }
