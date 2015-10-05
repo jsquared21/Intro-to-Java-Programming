37bc512de7dfc85288ee758382280b49de567c76
jsquared21 <jabarimail@gmail.com>
2015-10-05 12:16:28 -0400

    Add Ex 18.21

--- Exercise_18/Exercise_18_21/Exercise_18_21.java
+++ Exercise_18/Exercise_18_21/Exercise_18_21.java
@@ -33,9 +33,9 @@ public class Exercise_18_21 {
 
 	/** Recursive helper method */
 	public static String dec2Bin(int value, String result) {
-		if (value / 2 == 0)
+		if (value / 2 == 0) // Base case
 			return (value % 2) + result;
 		else
-			return dec2Bin(value / 2, (value % 2) + result);
+			return dec2Bin(value / 2, (value % 2) + result); // Recursive call
 	}
 }
