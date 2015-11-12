59b747895615c298156752b3e86a301188e6b833
jsquared21 <jabarimail@gmail.com>
2015-11-11 23:53:08 -0500

    Rewrite Ex 21.6 using a Map

--- Exercise_21/Exercise_21_06/Exercise_21_06.java
+++ Exercise_21/Exercise_21_06/Exercise_21_06.java
@@ -32,8 +32,11 @@ public class Exercise_21_06 {
 			}
 		}
 
-		System.out.print("The most occurrences integers are: ");
+		// Find the maximum occurrence
 		int max = Collections.max(map.values());
+
+		// Display the integers that have the most occurences
+		System.out.print("The most occurrences integers are: ");
 		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
 			if (entry.getValue() == max) {
 				System.out.print(entry.getKey() + " ");
