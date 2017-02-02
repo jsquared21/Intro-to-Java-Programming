dba80347edccec356c9f31bad2e68a3151bcbe12
jsquared21 <jabarimail@gmail.com>
2017-02-02 14:56:49 -0500

    Correct Ex06_19

--- Exercise_06/Exercise_06_19/MyTriangle.java
+++ Exercise_06/Exercise_06_19/MyTriangle.java
@@ -37,7 +37,7 @@ public class MyTriangle {
 	public static boolean isValid(
 		double side1, double side2, double side3) {
 		boolean valid =
-			side1 + side2 > side3 || side1 + side3 > side2 || side2 + side3 > side1;
+			side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
 		return valid;
 	}
 
