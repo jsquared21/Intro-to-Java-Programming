d2f27e4f46bcb0470e30b73ebdb7b0a3faf0d735
Joaquim Monserrat <qmonserrat@mailoo.org>
2018-01-26 12:59:20 +0100

    Display the first 50 random integers between 0 and 100, instead of displaying the first 50 random integers between 0 and 1000

--- Exercise_09/Exercise_09_04/Exercise_09_04.java
+++ Exercise_09/Exercise_09_04/Exercise_09_04.java
@@ -12,9 +12,9 @@ public class Exercise_09_04 {
 		// Displays the first 50 random integers between 0 and 100
 		for (int i = 1; i <= 50; i++) {
 			if (i % 10 == 0)
-				System.out.printf("%5d\n", randomNumber.nextInt(1000));
+				System.out.printf("%5d\n", randomNumber.nextInt(100));
 			else
-				System.out.printf("%5d", randomNumber.nextInt(1000));
+				System.out.printf("%5d", randomNumber.nextInt(100));
 		}
 	}
 }
