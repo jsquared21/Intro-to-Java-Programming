a738fac77356ee5ce1f321529f0ce8fad0647b55
Govind <govindchandak14@gmail.com>
2018-08-12 22:36:12 +0530

    Completed Exercise_03_15

--- Exercise_03/Exercise_03_15/Exercise_03_15.java
+++ Exercise_03/Exercise_03_15/Exercise_03_15.java
@@ -16,7 +16,7 @@ public class Exercise_03_15 {
 		Scanner input = new Scanner(System.in);
 
 		// Generate a random three-digit number
-		int lottery = (int)(Math.random() * 1000);
+		int lottery = 123;
 
 		// Prompt the user to enter a three-digit number
 		System.out.print("Enter a three-digit number: ");
@@ -30,18 +30,31 @@ public class Exercise_03_15 {
 
 		// Extract digits from guess
 		int guessDigit1 = guess / 100;
-		int remainingDigits = guess % 100;
+		remainingDigits = guess%100;
 		int guessDigit2 = remainingDigits / 10;
 		int guessDigit3 = remainingDigits % 10;
 
+		
 		System.out.println("The lottery number is " + lottery);
-
+		
 		// Check the guess
 		if (guess == lottery)
 			System.out.println("Exact match: you win $10,000");
-		if (guessDigit1 == lotteryDigit2)
-		{
-
-		}
+		else if ((guessDigit1==lotteryDigit2 && guessDigit2==lotteryDigit3 && guessDigit3==lotteryDigit1)||
+				(guessDigit1==lotteryDigit2 && guessDigit2==lotteryDigit1 && guessDigit3==lotteryDigit3)||
+				(guessDigit1==lotteryDigit3 && guessDigit2==lotteryDigit1 && guessDigit3==lotteryDigit2)||
+				(guessDigit1==lotteryDigit3 && guessDigit2==lotteryDigit2 && guessDigit3==lotteryDigit1)||
+				(guessDigit1==lotteryDigit1 && guessDigit2==lotteryDigit3 && guessDigit3==lotteryDigit2))
+				{
+					System.out.println("Match all digits: you win $3,000");
+				}
+		else if(guessDigit1==lotteryDigit1||guessDigit1==lotteryDigit2||guessDigit1==lotteryDigit3||
+				guessDigit2==lotteryDigit2||guessDigit2==lotteryDigit1||guessDigit2==lotteryDigit3||
+				guessDigit3==lotteryDigit3||guessDigit3==lotteryDigit1||guessDigit3==lotteryDigit2)
+				{
+					System.out.println("Match one digit: you win $1,000");
+				}
+		else
+			System.out.println("Sorry,no match");
 	}
 }
