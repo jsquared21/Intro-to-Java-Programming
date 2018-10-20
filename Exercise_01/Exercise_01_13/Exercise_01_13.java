f214c7f9fcb2999ca34aed4bb44fe9446e1376ab
Anantajit Subrahmanya <anantajit@users.noreply.github.com>
2018-10-19 19:39:40 -0700

    Use \t instead of repeatedly adding spaces... it's just better style

--- Exercise_01/Exercise_01_13/Exercise_01_13.java
+++ Exercise_01/Exercise_01_13/Exercise_01_13.java
@@ -15,14 +15,14 @@ public class Exercise_01_13 {
 		System.out.println("3.4x + 50.2y = 44.5");
 		System.out.println("2.1x +  .55y =  5.9");
 		System.out.println("x = (44.5 * .55) - (50.2 * 5.9)");
-		System.out.println("    ----------------------------");
-		System.out.println("    (3.4  * .55) - (50.2 * 2.1)");
+		System.out.println("\t" + "----------------------------");
+		System.out.println("\t" + "(3.4  * .55) - (50.2 * 2.1)");
 		System.out.println((((44.5 * .55) - (50.2 * 5.9)) / 
 							 ((3.4 * .55) - (50.2 * 2.1))));
 		System.out.println(" ");
 		System.out.println("y = (3.4 * 5.9) - (44.5 * 2.1)");
-		System.out.println("    --------------------------");
-		System.out.println("    (3.4 * .55) - (50.2 * 2.1)");
+		System.out.println("\t" + "--------------------------");
+		System.out.println("\t" + "(3.4 * .55) - (50.2 * 2.1)");
 		System.out.println((((3.4 * 5.9) - (44.5 * 2.1)) /
 							((3.4 * .55) - (50.2 * 2.1))));
 	}
